073638562e15ae73f409f81e83a33577b3049669
Arjun Karyappa <arjunkaryappa@Arjuns-MacBook-Air.local>
2023-04-22 15:24:04 +0530

    updated

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -3,12 +3,14 @@ const io =require('socket.io')(8000)
 const user={};
 
 io.on('connection',socket=>{
-    socket.on('new-user joined',name=>{
-        user[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
+    socket.on('new-user-joined',Name=>
+    {
+        console.log("new user",Name);
+        user[socket.id]=Name;
+        socket.broadcast.emit('user-joined',Name);
     })
     socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message,name,name: user[socket.id]})
+        socket.broadcast.emit('receive',{message: message,Name:user[socket.id]})
 
     })
 } )
